c57783bfe43af3ac9f27b3f0cc1b47c2a9c1f7c3
Solofeed <louismarie.guillemot@gmail.com>
2018-12-21 20:19:31 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,56 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const path = require('path');
+
 const isAnalyze = process.env.BUNDLE_ANALYZE;
 
 module.exports = {
-  chainWebpack: config => {
-    // aliases
-    config.resolve.alias.delete('@');
-    config.resolve.alias.set('@app', path.join(__dirname, 'src', 'app'));
-
-    // plugins
-    if (isAnalyze) {
-      config.plugin('analize').use(BundleAnalyzerPlugin);
-    }
-
-    config.plugin('copy').use(CopyWebpackPlugin, [
-      [
-        {
-          from: 'src/assets',
-          to: './assets'
-        }
-      ]
-    ]);
-    config.plugin('html').tap(args => {
-      args[0].template = path.join('src/index.html');
-      return args;
-    });
-  }
+  chainWebpack
 };
 
-function chainWebpack() {
-  chainAliases(config);
-  chainPlugins(config);
+function chainWebpack(config) {
+  /* aliases **********************************************************************************************************/
+
+  config.resolve.alias.delete('@');
+  config.resolve.alias.set('@app', path.join(__dirname, 'src', 'app'));
+
+  /* loaders **********************************************************************************************************/
+
+  // pug
+  config.module.rules.delete('pug');
+  config.module
+    .rule('pug')
+    .test(/\.pug$/)
+    .oneOf('vue')
+    .resourceQuery(/^\?vue/)
+    .use('pug-plain')
+    .loader('pug-plain-loader')
+    .end()
+    .end()
+    .oneOf('!vue')
+    .use('raw')
+    .loader('raw-loader')
+    .end()
+    .use('pug-plain')
+    .loader('pug-plain-loader');
+
+  /* plugins **********************************************************************************************************/
+
+  if (isAnalyze) {
+    config.plugin('analize').use(BundleAnalyzerPlugin);
+  }
+
+  config.plugin('copy').use(CopyWebpackPlugin, [
+    [
+      {
+        from: 'src/assets',
+        to: './assets'
+      }
+    ]
+  ]);
+
+  config.plugin('html').tap(args => {
+    args[0].template = path.join('src/index.pug');
+    return args;
+  });
 }
-
-function chainAliases(config) {}
-
-function chainPlugins(config) {}
